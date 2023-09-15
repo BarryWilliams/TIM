3e5641ac0266196ae3a773a0e5a093c323408d72
Barry Williams <barry.e.williams42@gmail.com>
2023-09-15 14:43:44 -0400

    Docker image name change

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-docker build . -t barrywilliams/k8s-ruby-sample-app
+docker build . -t barrywilliams/tim-sample-app
 
-
-#local run
-# docker run -p 4567:4567 barrywilliams/k8s-ruby-sample-app
--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker run -p 4567:4567 barrywilliams/k8s-ruby-sample-app
+docker run -p 4567:4567 barrywilliams/tim-sample-app
+
